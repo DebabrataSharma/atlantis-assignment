710334b7d5608acb78fa7ff49752e53fd113339f
Debabrata Sharma <sharmadeba92@gmail.com>
2021-08-10 13:02:46 +0530

    added assignment 1

---
+++ assignment-1/config.py
@@ -0,0 +1,7 @@
+github_link = "https://github.com/vinta/awesome-python"
+headers={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 \
+    Safari/537.36'
+    }
+timeout=6000
